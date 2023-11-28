7021ec500ce3e8c6dd34031670b8b46657e1d8b6
amac81 <arnaldocanelas@gmail.com>
2023-11-28 16:42:42 +0000

    vite.config.js update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://amac81.github.io/multistep_form_react/",
 })
