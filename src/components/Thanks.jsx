e073c2bff06258bc23639853a01ae852adc2f605
amac81 <arnaldocanelas@gmail.com>
2023-11-30 15:03:26 +0000

    Thanks component update

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -17,7 +17,7 @@ const reviewFeedbackEmojis = {
 const Thanks = ({data}) => {
   return (
     <div className="thanks-container">
-      <h2>Falta pouco <span>{data.name}</span></h2>
+      <h2>Obrigado <span>{data.name}</span>!</h2>
       <p>A sua opinião é muito importante para nós. Em breve receberá um cupão de desconto de 10% para aplicar na sua próxima compra.</p>
       <p>Para concluir a avaliação clique no botão enviar.</p>
       <h3>Resumo da sua avaliação:</h3>
