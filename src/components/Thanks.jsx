ee9c01320bc2176e0e38db2e23f0eef7f12ce560
amac81 <arnaldocanelas@gmail.com>
2023-11-30 12:41:45 +0000

    Thanks component created; CSS  update

--- src/components/Thanks.jsx
+++ src/components/Thanks.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+import "./Thanks.css"
 
 const Thanks = () => {
   return (
-    <h2>Thanks</h2>
+    <div className="thanks-container">
+      <h2>Falta pouco...</h2>
+      <p>A sua opinião é muito importante para nós. Em breve receberá um cupão de desconto de 10% para aplicar na sua próxima compra.</p>
+      <p>Para concluir a avaliação clique no botão enviar.</p>
+      <h3>Resumo da sua avaliação:</h3>
+
+    </div>
   )
 }
 
